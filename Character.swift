da2be7860f1fd6fdb35595e8dc4bfb632ae37d1b
Imanol Munoz <imanol72102@gmail.com>
2020-03-13 12:12:19 -0500

    added some of the basic stuff to save

--- Character.swift
+++ Character.swift
@@ -13,10 +13,39 @@ class PlayerCharacter {
     
     var name: String?
     var race: String?
-//    init(username: String?, userRace: String?) {
-//        name = username
-//        race = userRace
-//    }
+    var playerClass: String?
+    var playerStrength: Int?
+    var playerDexterity: Int?
+    var playerConstitution: Int?
+    var playerWisdom: Int?
+    var playerIntelligence: Int?
+    var playerCharisma: Int?
+    var playerLevel: Int?
+    var playerProficiency: Int?
+    var playerEquipment: String?
+    var equipmentCount: String?
+    var playerSpellSlots: Int?
+    var playerBackground: String?
+    
+    
+    
+    init(username: String?, userRace: String?, userClass: String?, userStrength: Int?, userDexterity: Int?, userConstitution: Int?, userWisdom: Int?, userIntelligence: Int?, userCharisma: Int?, userLevel: Int?, userProficiency: Int?, userEquipment: String?, userEquipmentCount: String?, userSpellSlots: Int?, userBackground: String?) {
+            name = username
+            race = userRace
+        playerClass = userClass
+            playerStrength = userStrength
+            playerDexterity = userDexterity
+            playerConstitution = userConstitution
+            playerWisdom = userWisdom
+            playerIntelligence = userIntelligence
+            playerCharisma = userCharisma
+            playerLevel = userLevel
+        playerProficiency = userProficiency
+        playerEquipment = userEquipment
+        equipmentCount = userEquipmentCount
+        playerSpellSlots = userSpellSlots
+        playerBackground = userBackground
+        }
     
     
     
